a836bca53fd4c2a6d19795c1a4c4d1f5013206c3
Nathan Laney <nathaniel.laney@gmail.com>
2023-02-24 21:19:46 -0500

    Completed Project 2

--- models/event.js
+++ models/event.js
@@ -18,10 +18,10 @@ const events = [
     title: 'Smelly Cat Roasteries Tasting Demo',
     category: 'Coffee Tasting',
     host_name: 'Smelly Cat Roasteries',
-    details: '',
+    details: 'Come try the trademark blends of Smelly Cat!',
     location: '514 E 36th Street, Charlotte, NC 28205',
-    start_datetime: '',
-    end_datetime: '',
+    start_datetime: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
+    end_datetime: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
     image: ''
 },
 {
@@ -29,10 +29,10 @@ const events = [
     title: 'FORM VS FUNCTION',
     category: 'Coffee Tasting',
     host_name: 'HEX ROASTERS',
-    details: '',
+    details: 'FORM AND FUNCTION. OUR TWO MOST FAMOUS ROASTS. SIDE BY SIDE.',
     location: '1824 Statesville Ave. 101, Charlotte, NC 28206 ',
-    start_datetime: '',
-    end_datetime: '',
+    start_datetime: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
+    end_datetime: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
     image: ''
 },
 {
@@ -40,10 +40,10 @@ const events = [
     title: 'Single-Origin vs. Blend',
     category: 'Coffee Tasting',
     host_name: 'Night Swim Coffee',
-    details: '',
+    details: 'Take a dip with Night Swim into the world of Coffee, learning about single origin roasts and blends.',
     location: '4500 Old Pineville Road, Charlotte, NC 28217',
-    start_datetime: '',
-    end_datetime: '',
+    start_datetime: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
+    end_datetime: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
     image: ''
 },
 {
@@ -51,10 +51,10 @@ const events = [
     title: 'Cappuccino Creations',
     category: 'Latte Art Masterclass',
     host_name: 'Community Matters Cafe',
-    details: '',
+    details: 'All about our favourite foamy foreign cup of cappuccino, the Italian classic.',
     location: '821 W 1st St, Charlotte, NC 28202',
-    start_datetime: '',
-    end_datetime: '',
+    start_datetime: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
+    end_datetime: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
     image: ''
 },
 {
@@ -62,10 +62,10 @@ const events = [
     title: 'Behind the Scenes with Amelies',
     category: 'Latte Art Masterclass',
     host_name: 'Amelies',
-    details: '',
+    details: 'We aren\'t just macarons!',
     location: '136 E. 36th St., Charlotte, NC 28205',
-    start_datetime: '',
-    end_datetime: '',
+    start_datetime: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
+    end_datetime: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
     image: ''
 },
 {
@@ -73,10 +73,10 @@ const events = [
     title: 'Learning Lattes',
     category: 'Latte Art Masterclass',
     host_name: 'Not Just Coffee',
-    details: '',
+    details: 'Come learn how to make a basic heart at this free event, or your money back!',
     location: '224 E 7th St, Charlotte, NC 28202',
-    start_datetime: '',
-    end_datetime: '',
+    start_datetime: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
+    end_datetime: DateTime.now().toLocaleString(DateTime.DATETIME_SHORT),
     image: ''
 }
 ];
@@ -99,7 +99,9 @@ exports.findAllCategories = function() {
 
 exports.save = function(event) {
     event.id = uuidv4()
-    event.createdAt = DateTime.now().toLocaleString(DateTime.DATETIME_SHORT)
+    event.start_datetime = DateTime.fromISO(event.start_datetime_pre).toLocaleString(DateTime.DATETIME_SHORT);
+    event.end_datetime = DateTime.fromISO(event.end_datetime_pre).toLocaleString(DateTime.DATETIME_SHORT);
+
     events.push(event);
 };
 
@@ -107,7 +109,13 @@ exports.updateById = function(id, newevent) {
     let event = events.find(event=>event.id === id);
     if (event){
         event.title = newevent.title;
-        event.content = newevent.content;
+        event.details = newevent.details;
+        event.category = newevent.category;
+        event.start_datetime = DateTime.fromISO(newevent.start_datetime_pre).toLocaleString(DateTime.DATETIME_SHORT);
+        event.end_datetime = DateTime.fromISO(newevent.end_datetime_pre).toLocaleString(DateTime.DATETIME_SHORT);
+        event.location = newevent.location;
+        event.host_name = newevent.host_name;
+
         return true;
     } else {
         return false;
